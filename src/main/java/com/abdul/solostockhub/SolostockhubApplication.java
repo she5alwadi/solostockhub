8a14dc54bb6fba9aa82c6f8d44fa808065657973
Abdul <she5.alwadi@gmail.com>
2026-06-22 11:52:53 -0400

    Initial Spring Boot project setup

---
+++ src/main/java/com/abdul/solostockhub/SolostockhubApplication.java
@@ -0,0 +1,13 @@
+package com.abdul.solostockhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolostockhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolostockhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdul/solostockhub/SolostockhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdul.solostockhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolostockhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
